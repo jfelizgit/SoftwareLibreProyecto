2f24ee32bfee350edfba679b673e7798d0ab19d7
jfelizgit <109927299+jfelizgit@users.noreply.github.com>
2022-08-15 21:27:42 -0400

    url home working

--- InventarioProj/InventarioApp/models.py
+++ InventarioProj/InventarioApp/models.py
@@ -7,3 +7,6 @@ class Inventario(models.Model):
     codigo = models.CharField(primary_key=True, max_length=10, serialize=False)
     articulo = models.CharField(max_length=255)
     cantidad = models.IntegerField()
+
+    def __str__(self):
+        return f'Artículo: {self.articulo} ({self.cantidad})'
