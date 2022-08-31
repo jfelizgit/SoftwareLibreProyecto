c89a9e622480bc927c6837545b7281bc750e479e
George <jorge.feliz42@gmail.com>
2022-08-31 18:30:30 -0400

    navbar usuarios

--- InventarioProj/InventarioApp/static/js/gestion.js
+++ InventarioProj/InventarioApp/static/js/gestion.js
@@ -3,8 +3,8 @@ const btnEliminar = document.querySelectorAll('.btnEliminar');
 
 btnEliminar.forEach(btn=> {
 btn.addEventListener('click', (e)=> {
-    const confimacion = confirm('Estás seguro de eliminar');
-    if (!confimacion){
+    const confirmacion = confirm('¿Estás seguro de eliminar?');
+    if (!confirmacion){
         e.preventDefault();
         }
     });
--- InventarioProj/InventarioProj/settings.py
+++ InventarioProj/InventarioProj/settings.py
@@ -120,13 +120,13 @@ TIME_ZONE = 'America/Santo_Domingo'
 
 USE_I18N = True
 
-USE_TZ = True
+#USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
